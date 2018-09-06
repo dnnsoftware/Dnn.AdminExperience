20c9dd85daa2b5041c79db048e921f91fda8aa64
Ben <zyhfish@163.com>
2018-09-06 11:26:42 +0800

    DNN-21317: use data-name attribute.

--- EditBar/Dnn.EditBar.UI/editBar/resources/ContentEditorManager/Js/ModuleManager.js
+++ EditBar/Dnn.EditBar.UI/editBar/resources/ContentEditorManager/Js/ModuleManager.js
@@ -48,8 +48,8 @@ if (typeof dnn === "undefined" || dnn === null) { dnn = {}; };
 
             $this.addClass('dnnModuleManager');
             $this.append(this._generateLayout());
-            $this.append(this._generatePaneLabel());
 
+            this._initPaneName();
             this._injectVisualEffects();
 
             this._handleEvents();
@@ -75,9 +75,11 @@ if (typeof dnn === "undefined" || dnn === null) { dnn = {}; };
             return $('<a href="#" class="addModuleHandler" aria-label="Add Module"><span></span></a><a href="#" class="addExistingModuleHandler" aria-label="Add Existing Module"><span></span></a>');
         },
 
-        _generatePaneLabel: function() {
-            var paneName = this.$pane.attr('id').replace('dnn_', '');
-            return $('<span class="dnnPaneLabel" />').html(paneName);
+        _initPaneName: function() {
+            if (!this.$pane.attr('data-name')) {
+                var paneName = this.$pane.attr('id').replace('dnn_', '');
+                this.$pane.attr('data-name', paneName);
+            }
         },
         
         _injectVisualEffects: function () {
