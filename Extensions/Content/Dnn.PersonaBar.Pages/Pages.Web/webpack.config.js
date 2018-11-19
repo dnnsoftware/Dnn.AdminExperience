fb3719bd5e268815e8c5f6d7a867970de0532640
valadas <info@danielvaladas.com>
2018-11-19 02:06:48 -0500

    I think this fixes most config issues

--- Extensions/Content/Dnn.PersonaBar.Pages/Pages.Web/webpack.config.js
+++ Extensions/Content/Dnn.PersonaBar.Pages/Pages.Web/webpack.config.js
@@ -1,39 +1,74 @@
-﻿const webpack = require("webpack");
+﻿const isAnalyze = process.env.NODE_ENV === "analyze";
+const isProduction = process.env.NODE_ENV === "production";
 const packageJson = require("./package.json");
 const path = require("path");
-const isProduction = process.env.NODE_ENV === "production";
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const webpack = require("webpack");
 const webpackExternals = require("dnn-webpack-externals");
+
 module.exports = {
     entry: "./src/main.jsx",
     output: {
-        path: "../admin/personaBar/scripts/bundles/",
+        path: path.resolve(__dirname, "../admin/personaBar/scripts/bundles/"),
         filename: "pages-bundle.js",
         publicPath: isProduction ? "" : "http://localhost:8080/dist/"
     },
     module: {
-        loaders: [
-            { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ["react-hot-loader", "babel-loader"] },
-            { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
-            { test: /\.css$/, loader: "style-loader!css-loader" },
-            { test: /\.(ttf|woff|gif|png)$/, loader: "url-loader?limit=8192" }
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: "eslint-loader",
+                enforce: "pre"
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: { 
+                    loader: "babel-loader", 
+                    options: { presets: ["@babel/preset-env", "@babel/preset-react"] } 
+                }
+            },
+            { 
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader"
+                },{
+                    loader: "css-loader"
+                },{
+                    loader: "less-loader"
+                }]
+            },
+            { 
+                test: /\.css$/,
+                use: [{
+                    loader: "style-loader"
+                },{
+                    loader: "css-loader"
+                }]            
+            },
+            { 
+                test: /\.(ttf|woff|gif|png)$/,
+                use: {
+                    loader: "url-loader?limit=8192"
+                }
+            }
         ],
-
-        preLoaders: [
-           { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "eslint-loader" }
-        ]
     },
-
     resolve: {
-        extensions: ["", ".js", ".json", ".jsx"],
-        root: [
+        extensions: ["*", ".js", ".json", ".jsx"],
+        modules: [
             path.resolve('./src'),          // Look in src first
             path.resolve('./node_modules')  // Last fallback to node_modules
         ]
     },
     externals: webpackExternals,
+    optimization: isProduction ? {
+        minimizer: [new UglifyJsPlugin()]
+    } : {
+
+        },
     plugins: isProduction ? [
-        new webpack.optimize.UglifyJsPlugin(),
-        new webpack.optimize.DedupePlugin(),
         new webpack.DefinePlugin({
             VERSION: JSON.stringify(packageJson.version),
             "process.env": {
