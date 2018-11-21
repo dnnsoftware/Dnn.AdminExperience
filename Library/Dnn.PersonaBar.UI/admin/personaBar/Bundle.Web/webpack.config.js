e12337bd8c41515049023810ed02b49979288f6a
Matt Rutledge <mrutledge@believegroup.com>
2018-11-21 14:51:48 -0700

    Upgraded to babel 7

--- Library/Dnn.PersonaBar.UI/admin/personaBar/Bundle.Web/webpack.config.js
+++ Library/Dnn.PersonaBar.UI/admin/personaBar/Bundle.Web/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     module: {
         rules:[
             { test: /\.(js|jsx)$/, enforce: "pre", exclude: /node_modules/, loader: "eslint-loader", options: { fix: true } },
-            { test: /\.(js|jsx)$/ ,  exclude: /node_modules/, loaders: ["babel-loader?presets[]=react"] },
+            { test: /\.(js|jsx)$/ , exclude: /node_modules/, loader: "babel-loader" },
             { test: /\.(less|css)$/, loader: "style-loader!css-loader!less-loader" },
             { test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/, loader: "url-loader?mimetype=application/font-woff" },
             { test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/, loader: "file-loader?name=[name].[ext]" },
