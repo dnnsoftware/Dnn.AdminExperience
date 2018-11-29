101f086861477e4cb104562c2f20d76ad7c40bcd
Matt Rutledge <mrutledge@believegroup.com>
2018-11-28 22:37:37 -0700

    Update packages and configs

--- Extensions/Manage/Dnn.PersonaBar.Sites/Sites.Web/.eslintrc.js
+++ Extensions/Manage/Dnn.PersonaBar.Sites/Sites.Web/.eslintrc.js
@@ -1,21 +1,38 @@
 module.exports = {
     "plugins": [
-        "react",
-        "spellcheck"
+        "react"
     ],
     "env": {
         "browser": true,
         "commonjs": true
     },
     "extends": ["eslint:recommended", "plugin:react/recommended"],
+    "settings": {
+        "react": {
+          "version": "16"
+        }
+    },
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true,
-            "experimentalObjectRestSpread": true
+            "arrowFunctions": true,
+            "blockBindings": true,
+            "classes": true,
+            "defaultParams": true,
+            "destructuring": true,
+            "forOf": true,
+            "generators": true,
+            "modules": true,
+            "objectLiteralComputedProperties": true,
+            "regexUFlag": true,
+            "regexYFlag": true,
+            "spread": true,
+            "superInFunctions": false,
+            "templateStrings": true
         },
-        "ecmaVersion": 6,
+        "ecmaVersion": 2018,
         "sourceType": "module"
-    },        
+    },           
     "globals": {
         "__": false,
         "Promise": false,
