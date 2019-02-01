3286c4e318604d33bf050eabd6761f0567785e69
Daniel Aguilera <daniel.aguilera@aurea.com>
2019-01-31 22:49:57 -0300

    Added unit tests for Dnn.PersonaBar.Security

--- Extensions/Settings/Dnn.PersonaBar.Security/Services/SecurityController.cs
+++ Extensions/Settings/Dnn.PersonaBar.Security/Services/SecurityController.cs
@@ -58,8 +58,26 @@ namespace Dnn.PersonaBar.Security.Services
     public class SecurityController : PersonaBarApiController
     {
         private static readonly ILog Logger = LoggerSource.Instance.GetLogger(typeof(SecurityController));
-        private readonly Components.SecurityController _controller = new Components.SecurityController();
+        private readonly Components.SecurityController _controller;
+        private readonly IPortalAliasController _portalAliasController;
         private const string BULLETIN_XMLNODE_PATH = "//channel/item";
+
+        public SecurityController()
+            : this(
+                new Components.SecurityController(),
+                PortalAliasController.Instance
+            )
+        {
+        }
+
+        internal SecurityController(
+            Components.SecurityController controller,
+            IPortalAliasController portalAliasController
+            )
+        {
+            _controller = controller;
+            _portalAliasController = portalAliasController;
+        }
 
         #region Login Settings
 
@@ -1064,7 +1082,7 @@ namespace Dnn.PersonaBar.Security.Services
 
         #region Helpers
 
-        private string AddPortalAlias(string portalAlias, int portalId)
+        internal string AddPortalAlias(string portalAlias, int portalId)
         {
             if (!String.IsNullOrEmpty(portalAlias))
             {
@@ -1073,11 +1091,11 @@ namespace Dnn.PersonaBar.Security.Services
                 {
                     portalAlias = portalAlias.Remove(0, portalAlias.IndexOf("://", StringComparison.Ordinal) + 3);
                 }
-                var alias = PortalAliasController.Instance.GetPortalAlias(portalAlias, portalId);
+                var alias = _portalAliasController.GetPortalAlias(portalAlias, portalId);
                 if (alias == null)
                 {
                     alias = new PortalAliasInfo { PortalID = portalId, HTTPAlias = portalAlias };
-                    PortalAliasController.Instance.AddPortalAlias(alias);
+                    _portalAliasController.AddPortalAlias(alias);
                 }
             }
             return portalAlias;
